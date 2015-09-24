52010f819680e65ba7f111ab8328303fcc3956b8
Alexis DUCERF <alexis.ducerf@gmail.com>
2015-09-24 11:30:28 +0200

    POO

---
+++ Design Patterns/POO/Equide.php
@@ -0,0 +1,75 @@
+<?php
+
+class Equide
+{
+    private $_legs;
+    private $_color;
+    private $_head;
+
+    /**
+     * Equide constructor.
+     * @param $_legs
+     * @param $_color
+     * @param $_head
+     */
+    public function __construct($_legs, $_color, $_head)
+    {
+        $this->_legs = $_legs;
+        $this->_color = $_color;
+        $this->_head = $_head;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLegs()
+    {
+        return $this->_legs;
+    }
+
+    /**
+     * @param mixed $legs
+     * @return $this
+     */
+    public function setLegs($legs)
+    {
+        $this->_legs = $legs;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->_color;
+    }
+
+    /**
+     * @param mixed $color
+     * @return $this
+     */
+    public function setColor($color)
+    {
+        $this->_color = $color;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHead()
+    {
+        return $this->_head;
+    }
+
+    /**
+     * @param mixed $head
+     * @return $this
+     */
+    public function setHead($head)
+    {
+        $this->_head = $head;
+        return $this;
+    }
+}
---
+++ Design Patterns/POO/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'Equide.php';
+
+$horse = new Equide(4, 'rouge', 2);
+
+$horse->setHead(1)
+    ->setColor('brown')
+    ->setLegs(3);
+
+
+echo '<pre>';
+var_dump($horse);
+echo '</pre>';
+
+$myLittlePony = new Equide(4, 'black', 1);
+
+echo '<pre>';
+var_dump($myLittlePony);
+echo '</pre>';
