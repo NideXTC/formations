2aea1951fd237359b0a038eceed701a93a1c9aff
NideXTC <alexis.ducerf@gmail.com>
2016-02-18 15:20:39 +0100

    json

---
+++ MajDev/exercices/ex81.js
@@ -0,0 +1,14 @@
+var students = [
+    {
+        name: 'Han',
+        first_name: 'solo'
+    },
+    {
+        name: 'Chew',
+        first_name: 'Bacca'
+    }
+];
+
+console.log(students);
+
+console.log(students[0].name);
---
+++ MajDev/exercices/ex82.js
@@ -0,0 +1,40 @@
+var json =
+{
+    students: [
+        {
+            name: 'Han',
+            first_name: 'solo'
+        },
+        {
+            name: 'Chew',
+            first_name: 'Bacca'
+        }
+    ],
+    teachers: [
+        {
+            name: 'Doc',
+            first_name: 'Brown'
+        },
+        {
+            name: 'Marty',
+            first_name: 'McFly'
+        }
+    ]
+};
+
+console.log(json);
+
+// Marty
+console.log(json.teachers[1].name);
+
+
+
+
+
+
+
+
+
+
+
+
