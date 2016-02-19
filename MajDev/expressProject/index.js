7097335d08a027f787ce490924f6f507cf839603
NideXTC <alexis.ducerf@gmail.com>
2016-02-19 10:00:42 +0100

    mongoose

--- MajDev/expressProject/index.js
+++ MajDev/expressProject/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-
+var Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost/test');
 
@@ -9,20 +9,47 @@ var server = app.listen(3000, function () {
     console.log('Hello');
 });
 
+var schema = new Schema({
+    name: {type: String, required: true},
+    firstName: {type: String, required: true},
+    email: {type: String, required: true},
+    createdOn: {type: Date, default: Date.now}
+});
+
+var User = mongoose.model('User', schema, 'users');
+
 app.get('/',  function (req, res) {
-    res.send('get');
+    User.find({},function(err, users){
+        if(!err){
+            res.send(users);
+        } else {
+            console.error(err);
+        }
+    });
 });
 
 app.put('/',  function (req, res) {
-    res.send('put');
+    // Modifier
 });
 
 app.delete('/',  function (req, res) {
-    var json = {bien : "bien?"};
-    res.json(json);
+    // supprimer
 });
 
 app.post('/',  function (req, res) {
-    res.send('post');
+    var jacob = new User({
+        name : 'jacob',
+        firstName : 'jacob',
+        email : 'jacob@jacob.com'
+    });
+
+
+    jacob.save(function(err){
+        if(err){
+            console.error(err);
+        }
+        res.end();
+    });
+
 });
 
