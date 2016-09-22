a4ff2f45a1ad030cf00efa65d2cfd4a0c0f18443
NideXTC <alexis.ducerf@gmail.com>
2016-09-22 11:43:49 +0200

    mongoose

--- NodeJS/exercices/express.js
+++ NodeJS/exercices/express.js
@@ -1,22 +1,50 @@
 const express = require('express');
 const session = require('express-session'); 
+const path = require('path');
+const mongoose = require('mongoose');
 
-var app = express(); 
+mongoose.connect('mongodb://localhost/chocolat');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
 app.use(session({
 	resave : false, 
 	saveUninitialized: true, 
 	secret: "coucou"
 })); 
 
-// GET 
+
 app.get('/', (req, res) => {
-	res.send(req.session.toto);
+	res.render('index');
 }); 
 
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.get('/liste', (req, res) => {
+	res.render('index');
+});
+
+app.get('/connect', (req, res) => {
+	res.render('connect');
+});
+
+app.get('/sub', (req, res) => {
+	res.render('sub');
+});
+
+app.post('/sub', (req, res) => {
+	// Ajouter l'utilisateur en BDD
+	// Redirection
+});
+
 // POST 
-app.post('/', (req, res) => {
+app.post('/liste', (req, res) => {
 	req.session.toto = "tata";
-	res.send('POST');
+	res.end(); 
 }); 
 
 
