7c68a1b8d25fcb4a58e1d07b47276d0a4d349dd9
NideXTC <alexis.ducerf@gmail.com>
2016-10-03 11:56:37 +0200

    socket.io

---
+++ NodeJS/potato/app/sockets/Base.js
@@ -0,0 +1,15 @@
+module.exports = function(io) {
+    io.on('connection', function (socket) {
+        console.log('New user');
+        // On envoie le nombre de personnes actuellement sur le socket à tout le monde (sauf la personne qui vient de se connecter)
+        socket.broadcast.emit('UserState', io.engine.clientsCount);
+        // On envoie le nombre de personnes actuellement sur le socket à la personne qui vient de se connecter
+        socket.emit('UserState', io.engine.clientsCount);
+
+        socket.on('disconnect', function () {
+            console.log('User gone');
+            // On prévient tout le monde qu'une personne s'est deconnectée
+            socket.broadcast.emit('UserState', io.engine.clientsCount);
+        });
+    });
+}; 
