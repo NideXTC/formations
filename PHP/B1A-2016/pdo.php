de72c0d23a856981aabf1a7981cbe806c4990b47
NideXTC <alexis.ducerf@gmail.com>
2017-01-03 12:16:47 +0100

    pdo php

--- PHP/B1A-2016/pdo.php
+++ PHP/B1A-2016/pdo.php
@@ -1,6 +1,11 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+
 /* Connexion à une base ODBC avec l'invocation de pilote */
-$dsn = 'mysql:dbname=b1a_ingesup;host=localhost';
+$dsn = 'mysql:dbname=b1a_ingesup;host=127.0.0.1';
 $user = 'root';
 $password = 'root';
 
@@ -9,3 +14,24 @@ try {
 } catch (PDOException $e) {
     echo 'Connexion échouée : ' . $e->getMessage();
 }
+
+// Préparer la requête et l'enregistrer pour le lancement
+//$stmt = $dbh->prepare('SELECT name, email FROM user');
+$stmt = $dbh->prepare('SELECT * FROM user WHERE name = \'Toto\' AND password = \'Titi\'; ');
+
+// Executer la requête -> retourne un boolean
+$stmt->execute();
+
+echo '<pre>';
+var_dump($stmt->fetchAll());
+echo '</pre>';
+
+
+
+/*
+$stmt = $dbh->prepare('SELECT * FROM user');
+$stmt->execute();
+echo '<pre>';
+var_dump($stmt->fetchAll());
+echo '</pre>';
+*/
--- PHP/B1A-2016/sql.md
+++ PHP/B1A-2016/sql.md
@@ -23,6 +23,38 @@ INSERT INTO user VALUES (NULL, 'Toto', 'Titi', 'a@a.fr');
 
 4 - Modifier l'utilisateur Toto et lui changer son mail en 'b@b.fr' (UPDATE + WHERE)
 
+```
+UPDATE user SET email = 'b@b.fr' WHERE name = 'Toto'; 
+```
+
 5 - Supprimer l'utilisateur Toto (DELETE + WHERE)
+
+```
+DELETE FROM user WHERE id = 4
+```
+
+6 - Ressortir tous les champs avec une LIMIT de 2 
+
+```
+SELECT * FROM user LIMIT 2; 
+```
+
+7 - Ressortir tous les champs triés par nom (ORDER BY)
+
+```
+SELECT * FROM user ORDER BY name DESC; // Décroissant
+SELECT * FROM user ORDER BY name ASC; // Croissant
+```
+
+
+8 - Ressortir tous les champs qui ont le nom 'Toto' et le mot de passe 'Titi' (AND)
+
+```
+SELECT * FROM user WHERE name = 'Toto' AND password = 'Titi'; 
+```
+
+
+
+
 
 
