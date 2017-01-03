6bb40aa17edc9954086c55df91794a0b32d7a012
NideXTC <alexis.ducerf@gmail.com>
2017-01-03 13:57:38 +0100

    subscription

---
+++ PHP/B1A-2016/inscription.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!empty($_POST)) {
+    //TODO insert code here 
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+    <input type="text" name="name" id="name" placeholder="name">
+    <input type="text" name="password" id="password" placeholder="password">
+    <input type="text" name="email" id="email" placeholder="email">
+    <input type="submit" value="Envoyer">
+</form>
+</body>
+</html>
