236f64734538e0cc3ade696056e89f5bf57c0530
NideXTC <alexis.ducerf@gmail.com>
2017-02-06 10:27:44 +0100

    delete

---
+++ PHP/B1B-2017/delete.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'connect.php';
+
+
+if (empty($_SESSION['connected'])) {
+    header('Location:login.php');
+}
+
+if(!empty($_GET['action']) && $_GET['action'] == 'del'
+    && !empty($_GET['id'])){
+    // SUPPRESSION DE L'UTILISATEUR
+}
+
+
+
+$stmt = $dbh->prepare('SELECT * FROM users;');
+$stmt->execute();
+$users = $stmt->fetchAll();
+?>
+<table border="1">
+    <tr>
+        <th>#</th>
+        <th>Nom</th>
+        <th>Email</th>
+        <th>Action</th>
+    </tr>
+
+    <?php
+    foreach ($users as $user){
+        echo '
+        <tr>
+            <td>'.$user['id'].'</td>
+            <td>'.$user['name'].'</td>
+            <td>'.$user['email'].'</td>
+            <td><a href="delete.php?action=del&id='.$user['id'].'">Supprimer</a></td>
+        </tr>';
+    }
+    ?>
+
+</table>
