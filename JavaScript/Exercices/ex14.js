f3609932cd2c4c82cda6613853a60a624ee2e4d8
NideXTC <alexis.ducerf@gmail.com>
2017-03-06 11:37:49 +0100

    1.4

---
+++ JavaScript/Exercices/ex14.js
@@ -0,0 +1,15 @@
+var i = 0;
+var j = 0;
+var interval = setInterval(function(){
+	console.log('coucou');
+	i++; 
+	if(i == 3){
+		clearInterval(interval);		
+	}
+},1000);
+
+
+var interval2 = setInterval(_ => {
+	console.log('coucou2');
+	if(++j == 3)clearInterval(interval2);		
+},1000);
