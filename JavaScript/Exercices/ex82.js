c22ce45cb2a70e7ff267f69b9aee6823f42b2ef2
NideXTC <alexis.ducerf@gmail.com>
2017-03-07 14:21:47 +0100

    json

---
+++ JavaScript/Exercices/ex82.js
@@ -0,0 +1,19 @@
+const people = {
+	"students": [{
+		"first_name": 'Han',
+		"name": 'Solo'
+	}, {
+		"first_name": 'Chew',
+		"name": 'Bacca'
+	}],
+	"teachers": [{
+		"first_name": 'Marty',
+		"name": 'McFly'
+	}, {
+		"first_name": 'Doc',
+		"name": 'Brown'
+	}]
+}; 
+
+
+console.log(people);
--- JavaScript/Exercices/ex83.js
+++ JavaScript/Exercices/ex83.js
@@ -1,1 +1,33 @@
+const people = {
+	"students": [{
+		"first_name": 'Han',
+		"name": 'Solo'
+	}, {
+		"first_name": 'Chew',
+		"name": 'Bacca'
+	}],
+	"teachers": [{
+		"first_name": 'Marty',
+		"name": 'McFly'
+	}, {
+		"first_name": 'Doc',
+		"name": 'Brown'
+	}]
+}; 
+
+
+console.log(people.teachers[0].first_name);
+console.log(people['teachers'][0]['first_name']);
+
+
+
+
+
+
+
+
+
+
+
+
 
