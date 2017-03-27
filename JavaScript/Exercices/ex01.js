9b7f12811653cbb838f6d4640c3263b0a9c8e4a2
NideXTC <alexis.ducerf@gmail.com>
2017-03-27 10:32:00 +0200

    first

---
+++ JavaScript/Exercices/ex01.js
@@ -0,0 +1,11 @@
+// Déclaration de variable
+const myVar = 4;
+
+// Déclaration de variable
+function titi(){
+  const myVar = 5;
+}
+
+titi();
+
+console.log(myVar);
---
+++ JavaScript/Exercices/ex02.js
@@ -0,0 +1,6 @@
+
+if(1 === true){
+    console.log('OK');
+} else {
+    console.log('NOK');
+}
