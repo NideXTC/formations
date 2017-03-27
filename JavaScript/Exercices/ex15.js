000f69a2efaba0db6fad3707dd49e6f40bcb9cd0
NideXTC <alexis.ducerf@gmail.com>
2017-03-27 12:19:27 +0200

    2.1

---
+++ JavaScript/Exercices/ex15.js
@@ -0,0 +1,8 @@
+// ES5
+setTimeout(function(){
+    console.log('coucou');
+}, 2000);
+
+
+// ES6
+setTimeout(_ => console.log('coucou'), 2000);
---
+++ JavaScript/Exercices/ex21.js
@@ -0,0 +1,5 @@
+const ar = [4,3,5,8];
+
+console.log(ar);
+console.log(ar[0]);
+console.log(ar.length);
