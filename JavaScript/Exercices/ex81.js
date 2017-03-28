215854bedaa3ffb439aeac46b9e389bffe3ab151
NideXTC <alexis.ducerf@gmail.com>
2017-03-28 11:52:24 +0200

    json

---
+++ JavaScript/Exercices/ex81.js
@@ -0,0 +1,20 @@
+const obj = [{
+    name: "Han",
+    first_name: "Solo"
+}, {
+    name: "Chew",
+    first_name: "Bacca"
+}];
+
+const obj2 = {
+    students: [
+        {
+            name: "Han",
+            first_name: "Solo"
+        }
+        , {
+            name: "Chew",
+            first_name: "Bacca"
+        }
+    ]
+};
